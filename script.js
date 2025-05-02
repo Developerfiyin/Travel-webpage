55dbdd81a0f4c5498c1d09203355ea3a217daeb2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-05-02 17:21:21 +0100

    added scroll reveal to header image img

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ navLinks.addEventListener("click", (e) => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", ri - menu - line);
 });
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+scrollReveal().reveal(".header__image img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
